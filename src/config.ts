4ec37ece6333d04b52b4f4750e0443ea40bdbf9b
DiabolicalGolem <122225197+DiabolicalGolem@users.noreply.github.com>
2025-02-14 21:25:49 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Jacob Rosenlund | Personal Website';
-export const SITE_DESCRIPTION = 'My personal site for portfolio, project documentation, and blog-posts.';
+export const SITE_TITLE = 'Clue';
+export const SITE_DESCRIPTION = 'A clue website';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
